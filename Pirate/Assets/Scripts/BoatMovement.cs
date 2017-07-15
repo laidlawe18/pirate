4cdf4e7ffa6728188bbb049d3a67ead29cb2e4e1
C-Toll <mcgartollc18@barringtonschools.org>
2017-07-15 17:13:09 -0400

    Added islands and boats

---
+++ Pirate/Assets/Scripts/BoatMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatMovement : MonoBehaviour {
+
+	Rigidbody2D rb2d;
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		print (Input.GetAxis ("Horizontal"));
+		rb2d.AddForce (transform.right * -1 * Input.GetAxis ("Vertical"));
+		Vector3 rotation = transform.rotation.eulerAngles;
+		rotation.z -= 0.5f * Input.GetAxis ("Horizontal");
+		transform.rotation = Quaternion.Euler(rotation);
+	}
+}
