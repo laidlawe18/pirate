ffd4df7f3b4064047530008d1c1c37a31928c90f
Eliot Laidlaw <laidlawe18@barringtonschools.org>
2017-07-16 12:21:11 -0400

    Added a basic control scheme

--- Pirate/Assets/Scripts/BoatMovement.cs
+++ Pirate/Assets/Scripts/BoatMovement.cs
@@ -2,20 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BoatMovement : MonoBehaviour {
+public class BoatMovement : PlayerControllable {
 
 	Rigidbody2D rb2d;
 	// Use this for initialization
 	void Start () {
 		rb2d = GetComponent<Rigidbody2D> ();
+        isActive = false;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		print (Input.GetAxis ("Horizontal"));
-		rb2d.AddForce (transform.right * -1 * Input.GetAxis ("Vertical"));
-		Vector3 rotation = transform.rotation.eulerAngles;
-		rotation.z -= 0.5f * Input.GetAxis ("Horizontal");
-		transform.rotation = Quaternion.Euler(rotation);
+        if (isActive)
+        {
+            rb2d.AddForce(transform.right * -1 * Input.GetAxis("Vertical"));
+            Vector3 rotation = transform.rotation.eulerAngles;
+            rotation.z -= 0.5f * Input.GetAxis("Horizontal");
+            transform.rotation = Quaternion.Euler(rotation);
+        }
+        
 	}
 }
--- Pirate/Assets/Scripts/PlayerControl.cs
+++ Pirate/Assets/Scripts/PlayerControl.cs
@@ -4,13 +4,40 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour {
 
+    PlayerControllable[] controllables;
+    public int currActive;
+
 	// Use this for initialization
 	void Start () {
-		
+        controllables = GetComponentsInChildren<PlayerControllable>();
+        currActive = 0;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void FixedUpdate () {
+        if (controllables.Length == 0)
+        {
+            currActive = -1;
+        }
+        else
+        {
+            if (Input.GetButtonDown("Switch") && Input.GetAxis("Switch") > 0)
+            {
+                controllables[currActive].isActive = false;
+                currActive += 1;
+                currActive %= controllables.Length;
+                controllables[currActive].isActive = true;
+            }
+            if (Input.GetButtonDown("Switch") && Input.GetAxis("Switch") < 0)
+            {
+                controllables[currActive].isActive = false;
+                currActive -= 1;
+                if (currActive < 0)
+                {
+                    currActive = controllables.Length - 1;
+                }
+                controllables[currActive].isActive = true;
+            }
+        }
+    }
 }
