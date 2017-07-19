fd118bc9f93d4ef4297aa48208db8ba53d469c19
C-Toll <mcgartollc18@barringtonschools.org>
2017-07-19 19:34:47 -0400

    DOCKS

--- Pirate/Assets/GameScripts/BoatMovement.cs
+++ Pirate/Assets/GameScripts/BoatMovement.cs
@@ -6,11 +6,14 @@ public class BoatMovement : PlayerControllable {
 
 	Rigidbody2D rb2d;
     Cannon[] cannons;
+    GameObject island;
+    public GameObject dock;
 	// Use this for initialization
 	void Start () {
 		rb2d = GetComponent<Rigidbody2D> ();
         cannons = GetComponentsInChildren<Cannon>();
         isActive = false;
+        island = null;
 	}
 	
 	// Update is called once per frame
@@ -31,6 +34,24 @@ public class BoatMovement : PlayerControllable {
                     }
                 }
             }
+            if (Input.GetButton("BuildDock") && island != null)
+            {
+                Vector2 point1 = new Vector2(0, 0);
+                Vector2 point2 = new Vector2(0, 0);
+                float min = 10000000;
+                Vector2[] pts = island.GetComponent<PolygonCollider2D>().GetPath(0);
+                for (int i = 0; i < pts.Length; i ++)
+                {
+                    float distance = Vector2.Distance(transform.position,pts[i] + (Vector2)island.transform.position) + Vector2.Distance(new Vector2(transform.position.x, transform.position.y), pts[(i + 1)%pts.Length] + (Vector2)island.transform.position);
+                    if (distance < min)
+                    {
+                        min = distance;
+                        point1 = pts[i] + (Vector2)island.transform.position;
+                        point2 = pts[(i + 1)%pts.Length] + (Vector2)island.transform.position;
+                    }
+                }
+                GameObject newDock = Instantiate(dock, new Vector2((point1.x + point2.x) / 2, (point1.y + point2.y) / 2), Quaternion.LookRotation(Vector3.forward, -(new Vector2((point2-point1).y,-(point2 - point1).x))));
+            }
         }
 
         
@@ -47,7 +68,13 @@ public class BoatMovement : PlayerControllable {
         if (other.gameObject.GetComponent<Island>() != null)
         {
             other.gameObject.GetComponent<Island>().InRange();
+            island = other.gameObject;
         }
+    }
+
+    private void CreateDock()
+    {
+
     }
 
     public void OnTriggerExit2D(Collider2D other)
--- Pirate/Assets/GameScripts/Island.cs
+++ Pirate/Assets/GameScripts/Island.cs
@@ -4,7 +4,6 @@ using UnityEngine.UI;
 using UnityEngine;
 
 public class Island : MonoBehaviour {
-
     Text text;
 
     // Use this for initialization
